0ffbe0f5995a4798daf93db1f6031e535dc34840
PirateGamers <109447641+PirateGamers@users.noreply.github.com>
2022-11-25 14:43:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -6,11 +6,11 @@ class script(object):
 \n 𝙹𝚞𝚜𝚝 𝙴𝚗𝚝𝚎𝚛 𝚃𝚑𝚎 𝙼𝚘𝚟𝚒𝚎 𝙽𝚊𝚖𝚎 𝚆𝚊𝚒𝚝 A 𝚜𝚎𝚌 𝚒𝚝 𝚠𝚒𝚕𝚕 𝚐𝚒𝚟𝚎 𝚝𝚑𝚎 𝙼𝚘𝚟𝚒𝚎😊 """
 
     HELP_TXT = """𝙷𝙴𝚈 {}
-How To Use Bot ? 🔘
-To Use Mdisk Shortner Link Movies Search Bot You Just Have A Account In https://mdiskshortnerlink.com/ref/Bichu From My Refferal |
-Go To Dashboard -> Menu -> Tools -> Devoloper Api . 
-Copy The Api And Add Popcorn Movies Search Bot In Group .
-To Connect Your Api Contact @PirateGamers He Will Verify And Give Access To You  .
+How To Use Bot Popcorn Movies Search Bot ? 🔘
+To Use Popcorn Movies Search Bot You Just Have A Account In https://omegalinks.in/ref/TechnicalBichu From My Refferal \n
+Go To Dashboard -> Menu -> Tools -> Devoloper Api . \n 
+Copy The Api And Add Popcorn Movies Search Bot In Group . \n
+To Connect Your Api Contact @PirateGamers He Will Verify And Give Access To You . /n
 Now Enjoy Your Profit 💸."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TechnicalBichu>✨️TechnicalBichu✨️</a>
