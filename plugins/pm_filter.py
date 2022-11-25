88d5a366d683b8e136f11f405efed63770c0c29c
PirateGamers <109447641+PirateGamers@users.noreply.github.com>
2022-11-25 14:46:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -618,10 +618,10 @@ async def auto_filter(client, msg, spoll=False):
     pre = 'filep' if settings['file_secure'] else 'file'
     api = await get_sundisk(msg.chat.id)
     if api is None:
-        api_url = f"https://t.me/{temp.U_NAME}?start="
+        api_url = f"https://OmegaLinks.in/st?api=9c53d31922826c891f8d5d730ef5c495c2bcf36e&url=https://t.me/{temp.U_NAME}?start="
     else:
         api = api["api"]
-        api_url = f"https://sundisk.in/st?api={api}&url=https://t.me/{temp.U_NAME}?start="
+        api_url = f"https://OmegaLinks.in/st?api={api}&url=https://t.me/{temp.U_NAME}?start="
     btn = [
         [
             InlineKeyboardButton(
