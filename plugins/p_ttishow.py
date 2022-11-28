6e4e51b30717dec6a62c34683067a6b7be4a9055
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-11-28 08:41:40 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -42,7 +42,7 @@ async def save_group(bot, message):
             InlineKeyboardButton(
                 'ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
             InlineKeyboardButton(
-                '📢 Updates', url='https://t.me/Technical_Bots')
+                '📢 Updates', url='https://t.me/mx_networks')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
